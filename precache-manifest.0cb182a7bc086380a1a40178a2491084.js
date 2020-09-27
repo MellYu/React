5eb9a85efbb0933b1d1f98889161fe8c12a2d233
MellYu <mel6273569@gmail.com>
2020-09-27 18:06:08 +0300

    Updates

---
+++ precache-manifest.0cb182a7bc086380a1a40178a2491084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6446704b32ce27e47ff2ede34849d2",
+    "url": "/framework-course/index.html"
+  },
+  {
+    "revision": "f6593f24e9b114baf114",
+    "url": "/framework-course/static/css/main.b884de4d.chunk.css"
+  },
+  {
+    "revision": "00c98c0096604cee3fb9",
+    "url": "/framework-course/static/js/2.8a5f0277.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/framework-course/static/js/2.8a5f0277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6593f24e9b114baf114",
+    "url": "/framework-course/static/js/main.da187e6c.chunk.js"
+  },
+  {
+    "revision": "032d2bf32f9cd7048061",
+    "url": "/framework-course/static/js/runtime-main.d8cb3e7f.js"
+  }
+]);
